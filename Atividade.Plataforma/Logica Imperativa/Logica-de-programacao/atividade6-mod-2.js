594b8335927050e9a672c2f56f7cd45fba482585
Mírian Menezes <81202092+Icognita@users.noreply.github.com>
2023-07-27 23:52:33 -0300

    Update atividade6-mod-2.js

--- Atividade.Plataforma/Logica Imperativa/Logica-de-programacao/atividade6-mod-2.js
+++ Atividade.Plataforma/Logica Imperativa/Logica-de-programacao/atividade6-mod-2.js
@@ -14,7 +14,7 @@ function calculadora(numero1, numero2, operacao){
 }
 let numero1=parseInt(prompt("Digite o primeiro número "));
 let numero2 =parseInt(prompt("Digite o segundo número "));
-let operacao=parseInt(prompt("Digite a operaçao desejada: 1-soma | 2-subtração | 3-Multiplicação | 4-Divisao  "));
+let operacao=parseInt(prompt("Digite a operaçao desejada: 1-soma | 2-Subtração | 3-Multiplicação | 4-Divisao  "));
 
 let resultado= calculadora(numero1, numero2, operacao);     
 console.log("o resultado da operação é:  ",resultado);

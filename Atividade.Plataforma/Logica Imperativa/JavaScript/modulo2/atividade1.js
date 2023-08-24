3004974013befa5c179636c18cf06dd514e108a9
Icognita <mirianvir@hotmail.com>
2023-08-24 00:27:01 -0300

    Atividade1 JavaScript   modulo 2

---
+++ Atividade.Plataforma/Logica Imperativa/JavaScript/modulo2/atividade1.js
@@ -0,0 +1,7 @@
+/**
+ * 
+ * Algumas vantagens de usar o ECMAScript 6, especialmente 
+ * para iniciantes já que é uma linguagem mais fácil de ser entendida,
+ *  possui inúmeras bibliotecas e frameworks (React, React Native, Angular 2, js, Meteor, etc), tem rapidez de leitura e execução, suporte para diversos navegadores, código leve, e outros. Em compensação, existem poucos recursos voltados para segurança da informação,
+ *  então esse deve ser um ponto de atenção.
+ */

a7b480dff910e3e685458cad783d5c036bb5f693
Icognita <mirianvir@hotmail.com>
2023-08-23 23:38:21 -0300

    Atividade 4 JavaScript

---
+++ Atividade.Plataforma/Logica Imperativa/JavaScript/atividade4.js
@@ -0,0 +1,14 @@
+ console.log("Cadastro");
+
+ let fullName="Mirian Menezes";
+ console.log(typeof(fullName));
+ let salary=7.000.toFixed(3);
+ console.log(typeof(salary));
+ let ege=30;
+ console.log(typeof(ege));
+
+ let diploma=true;
+ console.log(typeof(diploma));
+
+ console.log("cadastrado de Aluno:");
+console.log(`Aluno(a):  ${fullName}\n Idade: ${ege} \n Salario: ${salary} \n Tem Diploma? ${diploma}`);

a11738aa2146b0980452fb8baf8503ede24e26cd
Icognita <mirianvir@hotmail.com>
2023-08-23 23:59:20 -0300

    Atividade 5 JavaScript

---
+++ Atividade.Plataforma/Logica Imperativa/JavaScript/atividade5.js
@@ -0,0 +1,9 @@
+console.log("Calculadora de média");
+
+let nota1=Number(prompt("Digite sua primeira nota"));
+let nota2=Number(prompt("Digite sua segunda nota"));
+let nota3=Number(prompt("Digite sua terceira nota"));
+let media=((nota1+nota3+nota3)/3);
+console.log(media.toFixed(2));
+
+

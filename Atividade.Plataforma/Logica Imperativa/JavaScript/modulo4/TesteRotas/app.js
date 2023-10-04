720ae24f8980a477002a5c4c1edeb35ec782ea40
Icognita <mirianvir@hotmail.com>
2023-10-04 01:13:30 -0300

    ok

--- Atividade.Plataforma/Logica Imperativa/JavaScript/modulo4/TesteRotas/app.js
+++ Atividade.Plataforma/Logica Imperativa/JavaScript/modulo4/TesteRotas/app.js
@@ -1,13 +1,32 @@
+require('dotenv').config();
 const express=require("express")
-
-const bd= require("mysql")
-
+const mysql2= require("mysql2")
 const app=express()
 app.use(express.json())
+
+
 const routes= require('./routes')
 app.use('/',routes)
 
 const PORT=3000
+//conexao  com o MySQL
+
+const db= mysql2.createConnection({
+    host:process.env.db_HOST,
+    user:process.env.db_USER,
+    password:process.env.db_PASSWORD,
+    database:process.env.db_DATABASE
+})
+
+
+db.connect((error)=>{
+    if (error){
+        console.log("Erro ao conectar com o banco de dados", error)
+    } else{
+        console.log("Banco de dados conectado com sucesso")
+    }
+})
+
 
 app.get("/",(req,res)=>{
     res.send("Bem Vindo!")
